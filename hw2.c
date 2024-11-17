703fbc6425edd4a464281b607e72ac9986333df0
EastOst <deltak2022@gmail.com>
2024-11-17 17:55:12 +0900

    finish1

--- hw2.c
+++ hw2.c
@@ -269,155 +269,217 @@ void menu1() {
     else if (two_pair(mycard)) printf("투 페어\n");
     else if (one_pair(mycard)) printf("원 페어\n");
     else printf("노 페어");
+    printf("===============================================================\n");
 
 }
 
-
-
-void make_random(struct card mycard[size]) {
-    
-    srand(time(NULL));
-    for (int i =0;i<size;i++) {
-        int s =rand()%4;
-        switch (s) {
-            case 0: mycard[i].shape = 'S'; break;
-            case 1: mycard[i].shape = 'D'; break;
-            case 2: mycard[i].shape = 'C'; break;
-            case 3: mycard[i].shape = 'H'; break;
+void deck(struct card all[]) { //한번만 실행
+    char shapes[] = {'S', 'D', 'C', 'H'};
+    char *nums[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    int idx =0;
+    for(int i=0;i<4;i++) {
+        for(int j=0;j<13;j++) {
+            all[idx].shape=shapes[i];
+            strcpy(all[idx].num, nums[j]);
+            idx++;
         }
-        int ran_n = rand()%13 +2;
-        if (ran_n == 2) strcpy(mycard[i].num,"2");
-        else if(ran_n == 3) strcpy(mycard[i].num,"3");
-        else if(ran_n == 4) strcpy(mycard[i].num,"4");
-        else if(ran_n == 5) strcpy(mycard[i].num,"5");
-        else if(ran_n == 6) strcpy(mycard[i].num,"6");
-        else if(ran_n == 7) strcpy(mycard[i].num,"7");
-        else if(ran_n == 8) strcpy(mycard[i].num,"8");
-        else if(ran_n == 9) strcpy(mycard[i].num,"9");
-        else if(ran_n ==10) strcpy(mycard[i].num,"10");
-        else if(ran_n == 11) strcpy(mycard[i].num,"J");
-        else if(ran_n == 12) strcpy(mycard[i].num,"Q");
-        else if(ran_n == 13) strcpy(mycard[i].num,"K");
-        else  strcpy(mycard[i].num,"A");
-    //이후 구조체 포인터째로 전달
-
+    }
+}
+void make_card(struct card all[], struct card mycard[]) {
+    int check[5]={-1,-1,-1,-1,-1};
+    for(int i=0;i<5;i++) {
+        while (1) {
+        check[i] = rand()%52;
+        for(int j=0;j<i;j++) {
+            if (check[i]==check[j]) break;
+        }
+        break;
+        }
+        mycard[i] = all[check[i]];
+    }
     
-        
-    }   
 }
 
-void check(int select) {
-    clock_t start_clock, end_clock, diff_clock, ex_time;
-    int count = 0;
-    struct card mycard[5];
-    make_random(mycard);
-    start_clock = clock();
-    switch (select)
-    {
-    case 1: {while (1){
-        int a = royal_straight_flush(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    case 2: {while (1){
-        int a = back_straight_flush(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    case 3: {while (1){
-        int a = straight_flush(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    case 4: {while (1){
-        int a = four_card(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    case 5: {while (1){
-        int a = full_house(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    case 6: {while (1){
-        int a = flush(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    case 7: {while (1){
-        int a = mountain(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    case 8: {while (1){
-        int a = back_straight(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    case 9: {while (1){
-        int a = straight(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    case 10: {while (1){
-        int a = triple(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    case 11: {while (1){
-        int a = two_pair(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    case 12: {while (1){
-        int a = one_pair(mycard);
-        if (a==0) {make_random(mycard); count++;}
-        else {end_clock = clock();break;}
-    }
-    break;
-    }
-    }
+int check_jokbo(_Bool(*type)(struct card[]), struct card mycard[]) {
+    int a = type(mycard);
+    if (a==1) return 1;
+    else return 0;
+    
 
-    printf("생성된 카드: ");
-    for(int i = 0; i < 5; i++) {
-        printf("%c %s", mycard[i].shape, mycard[i].num);
-        if (i < 4) printf(", "); 
-}
-    printf("\n");
-    printf("컴퓨터로 1번 족보를 생성하기 위한 카드 생성 시도 : %d번\n",count);
-    diff_clock = end_clock - start_clock;
-    ex_time = diff_clock / CLOCKS_PER_SEC;
-    printf("생성에 걸린 시간 %lu초", ex_time);
 }
 
 void menu2() {
+    clock_t start_clock, end_clock, diff_clock, ex_time;
+    struct card all[52];
+    deck(all);
     printf("1. royal_straight_flush\t2. back_straight_flush\n3. straight_flush\t4. four_card\n5. full_house\t6. flush\n7. mountain\t8. back_straight\n9. straight\t10. triple\n11. two_pair\t12. one_pair\n");
     printf("무슨 족보를 생성할까요? ");
     int select;
     scanf("%d",&select);
-    check(select);
+
+    struct card mycard[size];
+    int count =1;
+    int check=0;
+    start_clock = clock();
+    make_card(all,mycard);
+    switch(select) {
+        case 1: {
+            check =check_jokbo(royal_straight_flush,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check=check_jokbo(royal_straight_flush,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+        case 2:
+        {
+            check =check_jokbo(back_straight_flush,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check=check_jokbo(back_straight_flush,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+        case 3:
+        {
+            check =check_jokbo(straight_flush ,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check=check_jokbo(straight_flush,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+        case 4:
+        {
+            check =check_jokbo(four_card ,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check=check_jokbo(four_card,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+        case 5:
+        {
+            check =check_jokbo(full_house  ,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check=check_jokbo(full_house ,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+        case 6:
+        {
+            check =check_jokbo(flush ,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check=check_jokbo(flush,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+        case 7:
+        {
+            check =check_jokbo(mountain  ,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check= check_jokbo(mountain ,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+        case 8:
+        {
+            check =check_jokbo(back_straight,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check=check_jokbo(back_straight,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+        case 9:
+        {
+            check =check_jokbo(straight ,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check=check_jokbo(straight ,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+        case 10:
+        {
+            check =check_jokbo(triple ,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check =check_jokbo(triple,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+        case 11:
+        {
+            check =check_jokbo(two_pair ,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check=check_jokbo(two_pair,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+        case 12:
+        {
+            check =check_jokbo(one_pair ,mycard);
+            while (check!=1) {
+                make_card(all,mycard);
+                count++;
+                check=check_jokbo(one_pair,mycard);
+                
+            }
+            end_clock = clock();
+            break;
+        }
+    }
+    printf("생성된 카드: ");
+    for(int i=0;i<size;i++) {
+        printf("%c %s",mycard[i].shape,mycard[i].num);
+        if(i<4) printf(",");
+    }
+    diff_clock = end_clock - start_clock;
+    ex_time = diff_clock / CLOCKS_PER_SEC;
+    printf("\n컴퓨터로 %d번 족보를 생성하기 위한 카드 생성 시도 : %d번\n",select,count);
+    printf("생성에 걸린 시간: %lu초\n",ex_time);
+    printf("===============================================================\n");
+     //카드 52장 생성 이후 menu2안에서 첫 카드 5장 생성하고 체크족보 함수 직접 호출. 호출하는 순간 시간 재기 
+
+    
     
 }
 
@@ -426,7 +488,7 @@ int main(void) {
     int menu;
     while(1) {
     printf("1. 직접 입력 2. 랜덤 생성 3. 종료\n");
-    printf("번호를 입력하세요: ");
+    printf("\n번호를 입력하세요: ");
     scanf("%d",&menu);
     getchar();
 
